9a46882bc0c06bb11ca1dc56a84d8cd1a5e890b1
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2018-04-06 18:00:37 -0500

    --quiet --set-upstream options for push doesn't work

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,10 @@
 # permisos de accesos a repositorios públicos.
 # /usr/lib64/ruby/gems/2.1.0/gems/travis-1.8.8/bin/travis encrypt GH_TOKEN="secretvalue_github_token"
 # link: https://docs.travis-ci.com/user/environment-variables/
+REMOTE_REPO_SLUG="$REMOTE_REPO_OWNER/$REMOTE_REPO_NAME"
+
 add_repo() {
   #rm -rf ${REMOTE_REPO_NAME} # repo never exists in travis
-  REMOTE_REPO_SLUG="$REMOTE_REPO_OWNER/$REMOTE_REPO_NAME"
   git clone -b gh-pages --depth 10 https://github.com/${REMOTE_REPO_SLUG}
   pushd ${REMOTE_REPO_NAME}
   # GH_ADMINAGENCIAIMPL_ACCESS_TOKEN is stablished in travis interface as private env var
@@ -23,12 +24,12 @@ add_files() {
   cp -r ../en/_build/ en
   cp -r ../es/_build/ es
   git add .
-  git commit --all --message "Travis build: $TRAVIS_BUILD_NUMBER, Commit: $TRAVIS_COMMIT, Remote Repo: https://github.com/$TRAVIS_REPO_SLUG"
+  git commit --all --message "Travis build: $TRAVIS_BUILD_NUMBER, Commit: $TRAVIS_COMMIT, Remote Repo: https://github.com/$REMOTE_REPO_SLUG"
   popd
 }
 
 upload_files() {
-  git push --quiet --set-upstream origin-pages gh-pages
+  git push origin-pages gh-pages
 }
 
 add_repo
