7bc812d091591656bc68097429b95370988055cb
wfpinedar <wfpinedar@gmail.com>
2018-04-19 16:48:22 -0500

    Fix for automatic publish found

--- generate.sh
+++ generate.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -eux
 
 cd src
-sphinx-versioning build src build/en --show-banner -- -D language='en'
-make -e SPHINXOPTS="-D language='en'" LANG='en' qthelp
+sphinx-versioning build src build/en/html --show-banner -- -D language='en'
+make -e SPHINXOPTS="-D language='en'" LANG='en' FORMAT='' qthelp
 qcollectiongenerator build/en/qthelp/*.qhcp
 #assistant -collectionFile build/en/qthelp/LADM_COLAssistant.qhc
 
-sphinx-versioning build src build/es --show-banner -- -D language='es'
-make -e SPHINXOPTS="-D language='es'" LANG='es' qthelp
+sphinx-versioning build src build/es/html --show-banner -- -D language='es'
+make -e SPHINXOPTS="-D language='es'" LANG='es' FORMAT='' qthelp
 qcollectiongenerator build/es/qthelp/*.qhcp
 #assistant -collectionFile build/es/qthelp/LADM_COLAssistant.qhc
